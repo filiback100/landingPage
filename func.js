ac9e3a181307b505089de081cde0b61fd0849452
filiback100 <filiback100@student.kau.se>
2023-02-22 14:52:06 +0100

    update

--- func.js
+++ func.js
@@ -1,8 +1,21 @@
 const bigWrapper = document.querySelector(".big-wrapper"); 
 const hamburgerMenu = document.querySelector(".hamburger-menu"); 
+let elementsArray = document.querySelectorAll(".close"); 
+ 
+
+
+
 hamburgerMenu.addEventListener("click", () =>{
   bigWrapper.classList.toggle("active");  
   // console.log(bigWrapper); 
   
 })
+ 
+elementsArray.forEach(function(elem){
+  elem.addEventListener("click", ()=> {
+    bigWrapper.classList.toggle("active"); 
+  })
+})
+
+
 
